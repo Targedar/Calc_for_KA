95349d8fd51940d3dfb28a70eeda2baa8cec2073
Targedar <goridem@yandex.ru>
2024-09-19 23:38:05 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -9,23 +9,25 @@ import (
 
 // Маппинг римских цифр на арабские
 var romanToArabic = map[string]int{
-	"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5,
-	"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
-	"XI": 11, "XII": 12, "XIII": 13, "XIV": 14, "XV": 15,
-	"XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20,
+	"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
 }
 
-// Маппинг арабских цифр на римские
-var arabicToRoman = []string{
-	"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-	"XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+// Маппинг для преобразования арабских чисел в римские
+var arabicToRoman = []struct {
+	Value  int
+	Symbol string
+}{
+	{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+	{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+	{1, "I"},
 }
 
 // Основная функция калькулятора
 func main() {
 	// Чтение строки с консоли
 	var input string
-	fmt.Println("Введите выражение (например, 2 + 2 или VI / III):")
+	fmt.Println("Введите выражение (например, 2 + 2 или IX + VIII):")
 	fmt.Scanln(&input)
 
 	// Удаляем лишние пробелы
@@ -43,7 +45,11 @@ func main() {
 	// Преобразование чисел в арабские для вычислений
 	num1, num2 := 0, 0
 	if isRoman {
-		num1, num2, err = convertRomanToArabic(operands[0], operands[1])
+		num1, err = romanToArabicConverter(operands[0])
+		if err != nil {
+			panic(err.Error())
+		}
+		num2, err = romanToArabicConverter(operands[1])
 		if err != nil {
 			panic(err.Error())
 		}
@@ -65,7 +71,8 @@ func main() {
 		if result <= 0 {
 			panic("В римской системе нет отрицательных чисел или нуля")
 		}
-		fmt.Println(arabicToRoman[result])
+		romanResult := arabicToRomanConverter(result)
+		fmt.Println(romanResult)
 	} else {
 		fmt.Println(result)
 	}
@@ -73,10 +80,8 @@ func main() {
 
 // Функция для разбора строки на операнды и оператор
 func parseInput(input string) ([]string, string, error) {
-	// Определение доступных операторов
 	operators := []string{"+", "-", "*", "/"}
 
-	// Поиск оператора в строке
 	var operator string
 	for _, op := range operators {
 		if strings.Contains(input, op) {
@@ -85,18 +90,15 @@ func parseInput(input string) ([]string, string, error) {
 		}
 	}
 
-	// Если оператор не найден, это ошибка
 	if operator == "" {
 		return nil, "", errors.New("некорректная операция")
 	}
 
-	// Разделение строки по оператору
 	operands := strings.Split(input, operator)
 	if len(operands) != 2 {
 		return nil, "", errors.New("некорректный формат операции")
 	}
 
-	// Убираем пробелы из операндов
 	for i := range operands {
 		operands[i] = strings.TrimSpace(operands[i])
 	}
@@ -106,24 +108,48 @@ func parseInput(input string) ([]string, string, error) {
 
 // Функция для проверки, используются ли римские цифры
 func isRomanNumerals(operands []string) bool {
-	_, ok1 := romanToArabic[operands[0]]
-	_, ok2 := romanToArabic[operands[1]]
-	if ok1 && ok2 {
+	_, ok1 := romanToArabicConverter(operands[0])
+	_, ok2 := romanToArabicConverter(operands[1])
+	if ok1 == nil && ok2 == nil {
 		return true
-	} else if ok1 || ok2 {
+	} else if ok1 == nil || ok2 == nil {
 		panic("используются одновременно римские и арабские цифры")
 	}
 	return false
 }
 
-// Функция для преобразования римских чисел в арабские
-func convertRomanToArabic(op1, op2 string) (int, int, error) {
-	num1, ok1 := romanToArabic[op1]
-	num2, ok2 := romanToArabic[op2]
-	if !ok1 || !ok2 {
-		return 0, 0, errors.New("некорректные римские числа")
+// Функция для конвертации римских чисел в арабские
+func romanToArabicConverter(roman string) (int, error) {
+	roman = strings.ToUpper(roman)
+	total := 0
+	prevValue := 0
+
+	for i := len(roman) - 1; i >= 0; i-- {
+		value, exists := romanToArabic[string(roman[i])]
+		if !exists {
+			return 0, errors.New("некорректные римские числа")
+		}
+
+		if value < prevValue {
+			total -= value
+		} else {
+			total += value
+		}
+		prevValue = value
 	}
-	return num1, num2, nil
+	return total, nil
+}
+
+// Функция для преобразования арабских чисел в римские
+func arabicToRomanConverter(num int) string {
+	result := ""
+	for _, val := range arabicToRoman {
+		for num >= val.Value {
+			result += val.Symbol
+			num -= val.Value
+		}
+	}
+	return result
 }
 
 // Функция для преобразования арабских чисел
